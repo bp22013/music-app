7b30a2e0eb0925ef07b015a486e233f4d3ad8243
bp22013 <118529334+bp22013@users.noreply.github.com>
2024-05-08 06:56:05 +0900

    Delete components directory

--- components/Conversation.tsx
+++
@@ -1,9 +0,0 @@
-export const Conversation = ({text}:{text:String})=>{
-    return (
-        <>
-            <div
-                className={"w-full h-[35px] border-black border hover:bg-black hover:text-white  cursor-pointer text-black mb-2 text-center flex justify-center items-center text-lg"}>{text}
-            </div>
-        </>
-    )
-}
