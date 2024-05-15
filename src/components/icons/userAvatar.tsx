64143e56e7cedd87a98863fb1a1bfa554f9c336d
bp22013 <118529334+bp22013@users.noreply.github.com>
2024-05-16 00:44:06 +0900

    Update userAvatar.tsx

--- src/components/icons/userAvatar.tsx
+++ src/components/icons/userAvatar.tsx
@@ -61,7 +61,7 @@ const UserAvatar = () => {
                         isBordered
                         as="button"
                         className="transition-transform"
-                        src={session.user_metadata.url}
+                        src={avatarSrc}
                     />
                 </DropdownTrigger>
                 <DropdownMenu aria-label="Profile Actions" variant="flat">
